113d67f072c3d49561451e465bf22ed551e00f58
sckinta <sckinta@gmail.com>
2023-01-31 23:00:20 -0500

    remove big html by code

--- convert2pdf.R
+++ convert2pdf.R
@@ -3,7 +3,7 @@ rmarkdown::render("cv_render.Rmd",
                   params = list(pdf_mode = FALSE),
                   output_file = "chun_su_cv.html")
 
-file.remove("resume_render.html")
+file.remove("chun_su_cv.html")
 # Knit the PDF version to temporary html location
 # use this to replace links with footnotes
 
